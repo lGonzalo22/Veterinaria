db8d8dcd6f19264049ba44e6ec5ce3b7f8338bab
carlosoherrera <141650289+carlosoherrera@users.noreply.github.com>
2023-10-18 11:26:38 -0300

    Veterinaria

--- src/Vistas/Menu.java
+++ src/Vistas/Menu.java
@@ -65,6 +65,11 @@ public class Menu extends javax.swing.JFrame {
         jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/Imagenes/mascotas (1).png"))); // NOI18N
 
         jMenuItem2.setText("Agregar Mascota");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
         jMenu2.add(jMenuItem2);
 
         jMenuBar1.add(jMenu2);
@@ -98,6 +103,10 @@ public class Menu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     /**
      * @param args the command line arguments
